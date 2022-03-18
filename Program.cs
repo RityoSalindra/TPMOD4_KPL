8efb0da92c384fc2ddb41ab8541d3fd73f662f1e
Rityo Salindra <nuruddin859@gmail.com>
2022-03-18 16:57:18 +0700

    menambahkan class DataGeneric

--- Program.cs
+++ Program.cs
@@ -1,2 +1,41 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tpmodul4_130
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            HaloGeneric<string> halo = new HaloGeneric<string>();
+            String x = "Rityo";
+            halo.SapaUser(x);
+            DataGeneric<String> data = new DataGeneric<string>("1302204014");
+            data.PrintData();
+
+        }
+    }
+    public class HaloGeneric<T>
+    {
+        public void SapaUser(T x)
+        {
+            Console.WriteLine("Halo " +x);
+        }
+    }
+
+    public class DataGeneric<T>
+    {
+        private T data;
+        public DataGeneric(T data)
+        {
+            this.data = data;
+        }
+        public void PrintData()
+        {
+            Console.WriteLine("Data yang disimpan adalah: " + this.data);
+        }
+    }
+}
